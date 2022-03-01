f6e5a131f9b7da979520ce6f16377bcab0bd9225
moana-yf <82370722+moana-yf@users.noreply.github.com>
2022-03-01 23:06:08 +0800

    Eslint update
    The code was buggy at line 18.

--- simp-pg/server.js
+++ simp-pg/server.js
@@ -15,14 +15,10 @@ const pusher = new Pusher({
 });
 
 app.use(cors());
-app.use(bodyParser, urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("port", process.env.PORT || 5000);
 
 const server = app.listen(app.get("port"), ()=> {
   console.log(`Express running -> PORT ${server.address().port}`);
 });
-
-
-
-
